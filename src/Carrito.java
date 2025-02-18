c877d6f0b67341aac64c2b3d4778138f5b878897
Your Name <you@example.com>
2025-02-18 19:58:31 +0100

    fd

--- src/Carrito.java
+++ src/Carrito.java
@@ -3,25 +3,8 @@ import java.util.Scanner;
 public class Carrito {
 
 	
-	public static void totalP(double[] precio,String[]nom) {
-		Scanner scan= new Scanner(System.in);
-		Scanner scan1= new Scanner(System.in);
-		String [] llista=new String[nom.length];
-		System.out.println("Selecciona producte");
-		String producte=scan.nextLine();
-		System.out.println("Cantitat");
-		double cant=scan1.nextDouble();
-		int posi=0;
-		for(int i=0;i<nom.length;i++) {
-			if(producte.equals(nom[i])) {
-				llista[posi]=nom[i];
-				double total=+precio[i]*cant;
-				
-			}
-		}
-		
-		
-		
+	
 	}
+	
 
-}
+
