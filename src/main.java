4ce265a1248be8d402d47ff16a97972f42ddcbfb
Silax17 <aparejat17@gmail.com>
2025-02-21 00:06:37 +0100

    main

--- src/main.java
+++ src/main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class main {
 	public static String[] hierba = { "Maria Juana", "Hierba de los bosques", "La seta feliz", "El oro verde" };
 	public static String[] origen = { "Mexico", "Andorra", "Marrueco", "Palestina" };
@@ -6,38 +8,61 @@ public class main {
 	// Variables publicas extra que considereis
 
 	public static void main(String[] args) {
+		Scanner scan= new Scanner(System.in);
 		String nombre, lugar;
 		double cantidad;
 		System.out.println("Buenos dias! Que quieres hacer hoy?");
+		System.out.println("1.Buscar producte per nom");
+		System.out.println("2.Buscar producte per origen");
+		System.out.println("3.Ordenar de menor a major");
+		System.out.println("4.Ordenar de major a menor");
+		System.out.println("5.Fer la llista de la compra");
+		System.out.println("6.Fer la llista de la compra");
+		System.out.println("7.Fer la llista de la compra");
+		System.out.println("8.Acabar");
+		String exit="";
+		String eleccio=scan.nextLine();
+		while(!exit.equals("no"))
+			if(eleccio.equals("1")) {
+				System.out.println("Usuario selecciona buscar por nombre");
+				// Codigo para buscar por nombre
+			}
+			else if(eleccio.equals("2")) {
+				System.out.println("Usuario selecciona buscar por origen");
+				lugar = "Andorra";
+				// Codigo para buscar por origen
+			}
 		
-		System.out.println("Usuario selecciona buscar por nombre");
-		
-		
-		// Codigo para buscar por nombre
-		
+			else if(eleccio.equals("3")) {
+				System.out.println("Usuario selecciona ordenar por precio de menor a mayor");
+				Buscar.OrdenarMenorMajor(precio,hierba);
 
-		System.out.println("Usuario selecciona buscar por origen");
-		lugar = "Andorra";
-		// Codigo para buscar por origen
+			}
+			else if(eleccio.equals("4")) {
+				System.out.println("Usuario selecciona ordenar por precio de menor a mayor");
+				Buscar.OrdenarMajorMenor(precio,hierba);
 
-		System.out.println("Usuario selecciona ordenar por precio de menor a mayor");
-		Buscar.OrdenarMenorMajor(precio,hierba);
+			}
 
-		System.out.println("Usuario selecciona ordenar por precio de menor a mayor");
-		Buscar.OrdenarMajorMenor(precio,hierba);
+			else if(eleccio.equals("5")) {
+				System.out.println("Usuario selecciona varios productos para comprar");
+				// Codigo para seleccionar primer producto
+				nombre = "Hierba de los bosques";
+				cantidad = 3;
 
-		System.out.println("Usuario selecciona varios productos para comprar");
-		// Codigo para seleccionar primer producto
-		nombre = "Hierba de los bosques";
-		cantidad = 3;
+				// Codigo para seleccionar segundo producto
+				nombre = "El oro verde";
+				cantidad = 1;
 
-		// Codigo para seleccionar segundo producto
-		nombre = "El oro verde";
-		cantidad = 1;
+				// Codigo para seleccionar tercer producto
+				nombre = "La seta feliz";
+				cantidad = 2.5;
+			}
+			else if(eleccio.equals("6")) {
+				
+			}
 
-		// Codigo para seleccionar tercer producto
-		nombre = "La seta feliz";
-		cantidad = 2.5;
+
 		
 		System.out.println("Usuario elimina el segundo elemento de su carrito");
 		// Codigo para eliminar elemento del carrito
